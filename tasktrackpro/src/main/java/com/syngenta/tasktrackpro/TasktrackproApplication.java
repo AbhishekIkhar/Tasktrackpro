bfe6331aa6e2e861f96fd42738374a1f6374f0c4
s1257955 <s1257955@INPUL5CG8514CZ4.APAC.SYNGENTA.ORG>
2024-01-24 21:25:48 +0530

    first commit of the project hurray!

---
+++ tasktrackpro/src/main/java/com/syngenta/tasktrackpro/TasktrackproApplication.java
@@ -0,0 +1,13 @@
+package com.syngenta.tasktrackpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasktrackproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasktrackproApplication.class, args);
+	}
+
+}
---
+++ tasktrackpro/src/test/java/com/syngenta/tasktrackpro/TasktrackproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syngenta.tasktrackpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasktrackproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
